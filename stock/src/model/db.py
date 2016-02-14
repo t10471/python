fa4352fc66996dc28ef6ebd65a3f74ec9c137a4e
t10471 <t104711202@gmail.com>
2016-02-14 03:23:18 +0000

    supported python3

--- stock/src/model/db.py
+++ stock/src/model/db.py
@@ -1,17 +1,15 @@
-#*- coding: utf-8 -*-
-
 import sqlalchemy
-from sqlalchemy import Column,TIMESTAMP,Integer,String,Text,Date,Float,ForeignKey
+from sqlalchemy import Column, TIMESTAMP, Integer, String, Text, Date, Float, ForeignKey,ForeignKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
-
 Base = declarative_base()
+
 
 class Stock(Base):
     __tablename__ = 'stock'
     code          = Column('code', Integer, primary_key=True, autoincrement=False)
     name          = Column('stock_name', Text)
-    market_code   = Column('market_code', String, ForeignKey('market.code'))
+    market_code   = Column('market_code', String(1), ForeignKey('market.code'))
     business_code = Column('business_code', Integer, ForeignKey('business.code'))
     create_time   = Column('create_time', TIMESTAMP ,default=sqlalchemy.func.now())
     market        = relationship('Market', backref=backref('market_code', order_by=market_code))
@@ -29,7 +27,7 @@ class Stock(Base):
 
 class StockDate(Base):
     __tablename__ =  'stock_date'
-    stock_code     = Column('stock_code', Integer, primary_key=True, autoincrement=False, ForeignKey('stock.code'))
+    stock_code     = Column('stock_code', Integer, ForeignKey('stock.code'), primary_key=True, autoincrement=False)
     date           = Column('date', Date, primary_key=True, autoincrement=False)
     start_price    = Column('start_price', Integer)
     max_price      = Column('max_price', Integer)
@@ -51,10 +49,10 @@ class StockDate(Base):
         self.create_time = create_time
     def __repr__(self):
         return "<StockDate('%s','%s', '%s', '%s', '%s', '%s', '%s','%s')>" % (self.stock_code,self.date, self.start_price, self.max_price,self.min_price,self.end_price,self.volume,self.create_time)
-#市場
+# 市場
 class Market(Base):
     __tablename__ = 'market'
-    code        = Column('code', Integer, primary_key=True, autoincrement=True)
+    code        = Column('code', String(1), primary_key=True)
     name        = Column('name', Text)
     create_time = Column('create_time', TIMESTAMP,default=sqlalchemy.func.now())
 
@@ -67,7 +65,7 @@ class Market(Base):
     def __repr__(self):
         return "<Stock('%s','%s', '%s')>" % (self.code, self.name,self.create_time)
 
-#業種
+# 業種
 class Business(Base):
     __tablename__ = 'business'
     business_code = Column('code', Integer, primary_key=True, autoincrement=True)
@@ -85,12 +83,15 @@ class Business(Base):
 
 class AnalysisDate(Base):
     __tablename__ = 'analysis_date'
-    stock_code   = Column('stock_code', Integer, primary_key=True, autoincrement=False, ForeignKey('stock_date.stock_code'))
-    date         = Column('date', Date, primary_key=True, autoincrement=False, ForeignKey('stock_date.date'))
-    group_id     = Column('group_id',Integer , primary_key=True, autoincrement=False, ForeignKey('analysis_group.id'))
-    value        = Column('value',Float )
+    stock_code   = Column('stock_code', Integer, primary_key=True, autoincrement=False)
+    date         = Column('date', Date, primary_key=True, autoincrement=False)
+    group_id     = Column('group_id',Integer, ForeignKey('analysis.id'), primary_key=True, autoincrement=False)
+    value        = Column('value',Float)
 
-    __table_args__ = {'mysql_engine':'InnoDB','mysql_charset':'utf8'}
+    __table_args__ = (
+        ForeignKeyConstraint(['stock_code', 'date'], ['stock_date.stock_code', 'stock_date.date']),
+        {'mysql_engine':'InnoDB','mysql_charset':'utf8'}
+    )
 
     def __init__(self, stock_code,date,group_id, value):
         self.stock_code   = stock_code
@@ -102,15 +103,15 @@ class AnalysisDate(Base):
 
 class Analysis(Base):
     __tablename__ = 'analysis'
-    id            = Column('id',Integer , primary_key=True, autoincrement=False) #@ReservedAssignment
-    parent_id     = Column('parent_id',Integer, ForeignKey('analysis.id') )
-    name          = Column('name',Text )
-    display_name  = Column('display_name',Text )
+    id            = Column('id',Integer , primary_key=True, autoincrement=True)
+    parent_id     = Column('parent_id',Integer, ForeignKey('analysis.id'))
+    name          = Column('name',Text)
+    display_name  = Column('display_name',Text)
 
     __table_args__ = {'mysql_engine':'InnoDB','mysql_charset':'utf8'}
 
-    def __init__(self, id, parent_id, name, display_name):  #@ReservedAssignment
-        self.id           = id  #@ReservedAssignment
+    def __init__(self, id, parent_id, name, display_name):
+        self.id           = id
         self.parent_id    = parent_id
         self.name         = name
         self.display_name = display_name
@@ -119,10 +120,10 @@ class Analysis(Base):
 
 class AnalysisParameter(Base):
     __tablename__ = 'analysis_parameter'
-    group_id      = Column('group_id',Integer , primary_key=True, autoincrement=False, ForeignKey('analysis.id'))
-    key           = Column('key',Text )
-    value         = Column('value',Text )
-    comment       = Column('comment',Text )
+    group_id      = Column('group_id',Integer, ForeignKey('analysis.id'), primary_key=True, autoincrement=False)
+    key           = Column('key',String(50), primary_key=True)
+    value         = Column('value',Text)
+    comment       = Column('comment',Text)
 
     __table_args__ = {'mysql_engine':'InnoDB','mysql_charset':'utf8'}
 
