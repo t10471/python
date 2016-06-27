75056454bfb49197eb44f6b4d6a1b0a0b4b408ec
t10471 <t104711202@gmail.com>
2016-06-27 02:47:03 +0000

    modify google

--- stock/src/google.py
+++ stock/src/google.py
@@ -6,7 +6,6 @@ import t10471.google.finance.getter as getter
 import t10471.google.finance.parser as parser
 import t10471.google.finance.register as register
 import t10471.google.finance.formatter as formatter
-from datetime import datetime, date
 import os
 import os.path as op
 import csv
@@ -15,26 +14,24 @@ import codecs
 TERM = 'term'
 DIR = 'dir'
 METHOD = 'method'
-MIN = {TERM: 'd', DIR: 'min', METHOD: 'get_min'}
-DAY = {TERM: 'd', DIR: 'day', METHOD: 'get_day'}
+MIN = {TERM: 'Y', DIR: 'min', METHOD: 'get_min'}
+DAY = {TERM: 'Y', DIR: 'day', METHOD: 'get_day'}
 
 class Google(object):
 
     def __init__(self):
-        url = 'http://www.google.com/finance/getprices?p={0}&f=d,h,o,l,c,v&i={1}&x={2}&q={3}&ts={4}'
+        url = 'http://www.google.com/finance/getprices?p={0}&f=d,h,o,l,c,v&i={1}&x={2}&q={3}'
         self.getter = getter.Getter(url)
         self.parser = parser.Parser()
         self.register = register.Register()
-        self.start = date(2000, 1, 1)
 
-    def run(self, term_type):
-        span = (date.today() - self.start).days
-        ts = datetime.now().strftime('%s')
+    def run(self, term_type, span):
+        span = str(span)
         in_data = self._parse_tsv(op.join(op.join(op.abspath(os.getcwd()), 'japan-all-stock-prices.tsv')))
         for code, symbol in in_data:
             print("code {0}, symbol {1}".format(code, symbol))
             method = getattr(self.getter, term_type[METHOD])
-            data = self.parser.parse(method(str(span), term_type[TERM], code, symbol, ts))
+            data = self.parser.parse(method(str(span), term_type[TERM], code, symbol))
             if len(data) != 0:
                 print(data[0][formatter.DATE])
             stock_dir = self._mkdir(op.join(op.abspath(os.getcwd()), 'output', 'stocks', term_type[DIR]))
@@ -52,4 +49,4 @@ class Google(object):
             return [('TYO', row['SC']) for row in csv.DictReader(fp, delimiter='\t')]
 
 if __name__ == '__main__':
-    Google().run(DAY)
+    Google().run(DAY, 10)
--- stock/src/t10471/google/finance/getter.py
+++ stock/src/t10471/google/finance/getter.py
@@ -1,4 +1,5 @@
 import urllib.request
+from urllib.error import HTTPError
 
 class Getter():
 
@@ -7,17 +8,20 @@ class Getter():
         self.interval_day = 86400
         self.interval_min = 300
 
-    def get_day(self, period, term, market, symbol, ts):
+    def get_day(self, period, term, market, symbol):
         period = period + term
-        return self.get(period, self.interval_day, market, symbol, ts)
+        return self.get(period, self.interval_day, market, symbol)
 
-    def get_min(self, period, term, market, symbol, ts):
+    def get_min(self, period, term, market, symbol):
         period = period + term
-        return self.get(period, self.interval_min, market, symbol, ts)
+        return self.get(period, self.interval_min, market, symbol)
 
-    def get(self, period, interval, market, symbol, ts):
-        url = self.url.format(period, interval, market, symbol, ts)
-        print(url)
-        with urllib.request.urlopen(url) as u:
+    def get(self, period, interval, market, symbol):
+        url = self.url.format(period, interval, market, symbol)
+        try:
+            u = urllib.request.urlopen(url)
             data = u.read()
+        except HTTPError as e:
+            print("ERROR {0}, {1}, {2} ".format(e.code, market, symbol))
+            raise e
         return data.decode('utf8').split('\n')
